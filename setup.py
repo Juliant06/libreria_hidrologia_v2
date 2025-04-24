4c51e2ed67b76194dc869173f26a8c7ff3ccb09f
JulianToro <jutoroar@unal.edu.co>
2025-04-24 14:42:43 -0500

    empaquetado del codigo

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hidro_tools',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    author='Your Name',
+    author_email='jutoroar@unal.edu.co',
+    description='Hidrological routines for hidrology design',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Juliant06/libreria_hidrologia_v2/tree/test_branch#',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
